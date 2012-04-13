76dd8f0bba653d841015898d22b096a67c967166
Jonathan Vingiano <jvingiano@gmail.com>
2012-04-13 17:29:39 -0400

    fixing link in labs page, concat all js

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -51,6 +51,52 @@ b).addClass("order-"+b)});b.thisElement.find(".pwText").each(function(a){b.data[
 c.hide();a.options.zIndex&&c.css("zIndex",a.options.zIndex);a.$el.bind({mouseover:function(){b(this).mousemove(function(a){c.css("backgroundPosition",a.pageX+"px "+a.pageY+"px")});c.css("backgroundImage","url("+b(this).attr("data-okimage")+")").show();a.options.fadeIn&&c.stop().fadeTo(a.options.fadeInDuration,1)},mouseout:function(){a.options.fadeOut?c.stop().fadeTo(a.options.fadeOutDuration,0,function(){b(this).css("backgroundImage","").hide()}):c.css("backgroundImage","").hide()}})};a.init()};b.okhover.options=
 {fadeIn:!1,fadeOut:!1,fadeInDuration:400,fadeOutDuration:400,zIndex:null};b.fn.okhover=function(d){return this.each(function(){new b.okhover(this,d)})}})(jQuery);
 
+
+/*
+ * OKType by OKFocus v1.0.0
+ * http://okfoc.us 
+ *
+ * Copyright 2012, OKFocus
+ * Licensed under the MIT license.
+ *
+ */
+
+(function(c){var e,b,f,g,h,i,j,k,l,m="100,200,300,400,500,600,700,800,900".split(","),n=["overline","line-through","underline"],o="arial,helvetica,arial black,comic sans ms,courier new,georgia,impact,monospace,times new roman,trebuchet ms,verdana,symbol,webdings,fantasy,cursive".split(",");c.oktype=function(d,p){var a=this;a.$el=c(d);a.el=d;a.init=function(){a.options=c.extend({},c.oktype.options,p);a.start()};a.start=function(){if(a.options.hover)if("function"===typeof c().on)a.$el.on({mouseenter:function(){a.hoverIn(c(this))},
+mouseleave:function(){a.hoverOut(c(this))}});else a.$el.bind({mouseenter:function(){a.hoverIn(c(this))},mouseleave:function(){a.hoverOut(c(this))}});else a.hoverIn(a.$el)};a.hoverIn=function(d){b=c(d);f=b.css("font-family");g=b.css("font-weight");h=b.css("font-size");i=b.css("color");j=b.css("text-transform");k=b.css("text-decoration");l=b.css("letter-spacing");a.options.hover&&b.addClass("oktype-hovering");e=window.setInterval(function(){b.css({"font-family":a.randomType(),"font-weight":a.options.weight?
+a.weight():b.css("font-weight"),"font-size":a.options.maxSize&&a.options.minSize?a.randomSize():b.css("font-size"),color:a.options.randomColor?a.randomColor():b.css("color"),"text-transform":a.options.toggleCase?a.toggleCase():b.css("text-transform"),"text-decoration":a.options.randomDecoration?a.randomDecoration():b.css("text-decoration"),"letter-spacing":a.options.minSpace&&a.options.maxSpace?a.randomSpacing():b.css("letter-spacing")})},a.options.interval)};a.hoverOut=function(){clearTimeout(e);
+a.options.hover&&b.removeClass("oktype-hovering");a.options.restore&&b.css({"font-family":f,"font-weight":g,"font-size":h,color:i,"text-transform":j,"text-decoration":k,"letter-spacing":l})};a.weight=function(){if("toggle"===a.options.weight)return a.randomBold();if("random"===a.options.weight)return a.randomWeight();console.log("OKType Error: You must set the option for weight to be either 'toggle' or 'random'.")};a.randomBold=function(){return 0==Math.round(1*Math.random())%2?"bold":"normal"};a.randomWeight=
+function(){return m[Math.floor(m.length*Math.random())]};a.toggleCase=function(){return 0==Math.round(1*Math.random())%2?"uppercase":"lowercase"};a.randomDecoration=function(){return n[Math.floor(n.length*Math.random())]};a.randomSpacing=function(){return Math.round(a.options.minSpace+Math.random()*(a.options.maxSpace-a.options.minSpace))+"px"};a.randomSize=function(){return Math.round(a.options.minSize+Math.random()*(a.options.maxSize-a.options.minSize))+"px"};a.randomColor=function(){return"#"+
+Math.floor(16777215*Math.random()).toString(16)};a.randomType=function(){return o[Math.floor(o.length*Math.random())]};a.init()};c.oktype.options={weight:null,minSize:null,maxSize:null,randomColor:null,randomDecoration:null,interval:100,toggleCase:null,maxSpace:null,minSpace:null,restore:!0,hover:!0};c.fn.oktype=function(b){return this.each(function(){new c.oktype(this,b)})}})(jQuery);
+
+/**
+ *
+ * OKSHADOW
+ *
+ **/
+
+(function(b){b.okshadow=function(c,g){var a=this;a.$el=b(c);a.el=c;a.$el.data("okshadow",a);a.init=function(){a.options=b.extend({},b.okshadow.options,g);a.build()};a.build=function(){a.start()};a.clamp=function(a,b,c){return Math.max(b,Math.min(c,a))};a.setoption=function(e,d){if(typeof e==="string"){if(a.options[e]=d,e==="color")return a.update()}else a.options=b.extend(a.options,e);a.mousemove(a)};a.start=function(){b(window).bind({mousemove:a.mousemove});a.mousemove({pageX:b(window).width()/2,
+pageY:b(window).height()/2});if(a.options.transparent)a.el.style.color="transparent"};a.mousemove=function(b){var d=a.$el.offset(),c=b.pageX,b=b.pageY,f=d.top+a.$el.height()/2,d=d.left+a.$el.width()/2-c;f-=b;sx=d/a.options.xFactor;sy=f/a.options.yFactor;distance=Math.sqrt(d*d+f*f);fuzz=distance/a.options.fuzz+a.options.fuzzMin;a.options.xMax!==null&&(sx=a.clamp(sx,-1*a.options.xMax,a.options.xMax));a.options.yMax!==null&&(sy=a.clamp(sy,-1*a.options.yMax,a.options.yMax));a.options.fuzzMax!==null&&
+(fuzz=a.clamp(fuzz,a.options.fuzzMin,a.options.fuzzMax));sx+=a.options.xOffset;sy+=a.options.yOffset;a.pageX=c;a.pageY=b;a.sx=sx;a.sy=sy;a.fuzz=fuzz;a.update()};a.update=function(){a.options.textShadow?a.$el.css("text-shadow",sx+"px "+sy+"px "+fuzz+"px "+a.options.color):a.$el.css("box-shadow",sx+"px "+sy+"px "+fuzz+"px "+a.options.color)};a.init()};b.okshadow.options={color:"#888",fuzz:40,fuzzMin:0,fuzzMax:null,xOffset:0,xFactor:30,xMax:null,yOffset:0,yFactor:30,yMax:null,textShadow:false,transparent:false};
+b.fn.okshadow=function(c){return this.each(function(){new b.okshadow(this,c)})}})(jQuery);
+
+
+
+/*
+
+  OKZoom by OKFocus
+  http://okfoc.us // @okfocus
+  Copyright 2012 OKFocus
+  Licensed under the MIT License
+
+*/
+
+$(function(c){var b=document.createElement("div");b.id="ok-lorgnette";b.style.position="absolute";b.style.backgroundRepeat="no-repeat";b.style.pointerEvents="none";b.style.display="none";b.style.zIndex=7879;document.body.appendChild(b);c.okzoom=function(d,e){var a=this;a.$el=c(d);a.el=d;a.$el.data("okzoom",a);a.init=function(){a.options=c.extend({},c.okzoom.options,e);a.el.onmouseover=a.build;a.el.onmousemove=a.mousemove;a.el.onmouseout=a.mouseout;a.options.height=a.options.height||a.options.width;
+var b=a.$el.data("okimage");a.has_data_image="undefined"!==typeof b;if(a.has_data_image)a.img=new Image,a.img.src=b};a.build=function(){if(!a.has_data_image)a.img=a.el;a.offset=a.$el.offset();a.width=a.$el.width();a.height=a.$el.height();a.options.scaleWidth?(a.naturalWidth=a.options.scaleWidth,a.naturalHeight=Math.round(a.img.naturalHeight*a.options.scaleWidth/a.img.naturalWidth)):(a.naturalWidth=a.img.naturalWidth,a.naturalHeight=a.img.naturalHeight);a.widthRatio=a.naturalWidth/a.width;a.heightRatio=
+a.naturalHeight/a.height;b.style.width=a.options.width+"px";b.style.height=a.options.height+"px";b.style.border=a.options.border;b.style.background=a.options.background+" url("+a.img.src+")";b.style.backgroundRepeat=a.options.backgroundRepeat;b.style.backgroundSize=a.options.scaleWidth?a.naturalWidth+"px "+a.naturalHeight+"px":"auto";b.style.borderRadius=b.style.OBorderRadius=b.style.MozBorderRadius=b.style.WebkitBorderRadius=a.options.round?a.options.width+"px":0;b.style.boxShadow=a.options.shadow};
+a.mousemove=function(c){var d=a.options.width/2,f=a.options.height/2,e=-1*Math.floor((c.pageX-a.offset.left)*a.widthRatio-d),g=-1*Math.floor((c.pageY-a.offset.top)*a.heightRatio-f);document.body.style.cursor="none";b.style.display="block";b.style.left=c.pageX-d+"px";b.style.top=c.pageY-f+"px";b.style.backgroundPosition=e+"px "+g+"px"};a.mouseout=function(){b.style.display="none";b.style.background="none";document.body.style.cursor="auto"};a.init()};c.okzoom.options={width:150,height:null,scaleWidth:null,
+round:!0,background:"#fff",backgroundRepeat:"no-repeat",shadow:"0 0 5px #000",border:0};c.fn.okzoom=function(b){return this.each(function(){new c.okzoom(this,b)})}});
+
+
 /**
  *
  * okjavascript
@@ -79,6 +125,16 @@ $(function(){
     $("#demo-hover").click(function(e){
     	$("#ok-bg").css('backgroundImage', '');
     });
+
+    $('#okzoom-test').okzoom({
+        scaleWidth: 300,
+        backgroundRepeat: "repeat",
+        width: 200,
+        height: 200
+    });
+
+    $('#oktype-test').oktype({ randomColor: true });
+
     
     var background = $('#bg'),
     hashes = [], 
@@ -118,9 +174,7 @@ $(function(){
         }
             
     });
-
-    
-
+   
     listItems.bind({ 
         mouseover: function() {
             var name = $(this).attr('id'), title = $(this).text();
@@ -188,24 +242,23 @@ $(function(){
     $("#title").live('click', remove);
     $("#bg, #p").bind('click', remove);
 
-});
 
-$(function(){
-$('#demo-shadow').okshadow({
-  xMax: 5,
-  yMax: 5,
-  fuzzMin: 1,
-  fuzzMax: 10,
-});
-$('#demo-shadow2').okshadow({
-  color: 'blue',
-  textShadow: true,
-  transparent: true,
-  xMax: 0,
-  yMax: 0,
-  fuzzMin: 1.5,
-  fuzz: 55
-});
+    $('#demo-shadow').okshadow({
+        xMax: 5,
+        yMax: 5,
+        fuzzMin: 1,
+        fuzzMax: 10,
+    });
+
+    $('#demo-shadow2').okshadow({
+        color: 'blue',
+        textShadow: true,
+        transparent: true,
+        xMax: 0,
+        yMax: 0,
+        fuzzMin: 1.5,
+        fuzz: 55
+    });
 });
 
 
